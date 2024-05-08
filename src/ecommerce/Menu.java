61d66ac454e477d6fcc5b4d855bdcb880bd1a6ee
Vany005 <vaniabeatriz999@gmail.com>
2024-05-08 16:43:18 -0300

    Optei por adicionar a estrutura Try Cath no Menu(sujeito a alterações

--- src/ecommerce/Menu.java
+++ src/ecommerce/Menu.java
@@ -1,5 +1,7 @@
 package ecommerce;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import cores.Cores;
@@ -9,8 +11,8 @@ public class Menu {
 
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
-		int opcao;
-		
+		int opcao = 0;
+	
 		while (true) {
 			System.out.println(Cores.TEXT_BLUE + Cores.ANSI_BLACK_BACKGROUND
 					+ "*****************************************************");
@@ -34,7 +36,15 @@ public class Menu {
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     " + Cores.TEXT_RESET);
 			
-			opcao = leia.nextInt();
+			try {
+				opcao = leia.nextInt();
+				
+			} catch(InputMismatchException e) {
+				System.out.println("\nPor favor, Digite uma opção valida!");
+				leia.nextLine();
+				opcao = 0;
+			}
+				
 			
 			if (opcao == 8) {
 				System.out.println("\nVolte Sempre!");
@@ -46,32 +56,43 @@ public class Menu {
 			switch (opcao) {
 			case 1:
 				System.out.println("Cadastrar Produto\n\n");
-
+				
+				keyPress();
 				break;
 			case 2:
 				System.out.println("Listar Produtos\n\n");
-
+				
+				keyPress();
 				break;
 			case 3:
 				System.out.println("Buscar Produto por ID\n\n");
-
+				
+				keyPress();
 				break;
 			case 4:
 				System.out.println("Carrinho\n\n");
-
+				
+				keyPress();
 				break;
 			case 5:
 				System.out.println("Cupons\n\n");
-
+				
+				keyPress();
 				break;
 			case 6:
 				System.out.println("Deletar\n\n");
-			
+				
+				keyPress();
+				break;
 			case 7:
 				System.out.println("Categoria\n\n");
-
+				
+				keyPress();
+				break;
 			default:
 				System.out.println("\nOpção Inválida!\n");
+				
+				keyPress();
 				break;
 		}
 	}
@@ -82,10 +103,21 @@ public class Menu {
 		System.out.println("Linkedin-Vania Beatriz");
 		System.out.println("github.github.com/Vany005");
 		System.out.println("*********************************************************");
-	}
+	} public static void keyPress() {
+
+		try {
+
+			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para continuar comprando...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+
+		}
 }
 
-	
+}
 	
 
 		
